1b4799de9acf036e25bf7710a2d2b3cf37e73764
Eguo Wang <wangeguo@gmail.com>
2025-05-05 19:12:58 +0800

    feat: implement main logic

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,16 @@ mod args;
 
 use std::{fs, process};
 
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context as _, Result};
 use clap::Parser as _;
+use cranelift::{
+    codegen::{print_errors::pretty_error, Context},
+    module::{default_libcall_names, Linkage, Module},
+    native::builder,
+    object::{ObjectBuilder, ObjectModule},
+    prelude::settings,
+};
+use cranelift_reader::{parse_test, ParseOptions};
 
 use self::args::Args;
 
@@ -31,8 +39,34 @@ fn main() {
 fn run() -> Result<()> {
     let args = Args::parse();
 
-    let _source = fs::read_to_string(&args.input)
+    let flag = settings::Flags::new(settings::builder());
+    let isa = builder().unwrap().finish(flag).unwrap();
+
+    let name = args.output.file_name().and_then(|s| s.to_str()).unwrap_or("a.out");
+    let builder = ObjectBuilder::new(isa.clone(), name, default_libcall_names()).unwrap();
+    let mut module = ObjectModule::new(builder);
+
+    let name = String::from(args.input.as_os_str().to_string_lossy());
+    let source = fs::read_to_string(&args.input)
         .context(format!("Failed to read input file: {}", args.input.display()))?;
+    let test_file = parse_test(&source, ParseOptions::default())
+        .with_context(|| format!("failed to parse {name}"))?;
+
+    for (func, _) in test_file.functions {
+        let mut context = Context::for_function(func);
+
+        // Compile
+        let _ = context
+            .compile(isa.as_ref(), &mut Default::default())
+            .map_err(|err| anyhow!("{}", pretty_error(err.func, err.inner)))?;
+
+        let name = context.func.name.to_string();
+        let fid = module.declare_function(&name, Linkage::Export, &context.func.signature)?;
+        module.define_function_with_control_plane(fid, &mut context, &mut Default::default())?;
+    }
+
+    let bytes = module.finish().emit()?;
+    fs::write(args.output, bytes)?;
 
     Ok(())
 }
